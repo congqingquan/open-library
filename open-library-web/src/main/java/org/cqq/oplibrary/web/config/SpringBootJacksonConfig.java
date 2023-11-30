6744a2c907389ba65c1aac22f7d3d385d679dba8
congqingquan <17600073620@163.com>
2023-11-30 10:51:30 +0800

    SpringBootJacksonConfig

--- open-library-web/src/main/java/org/cqq/oplibrary/web/config/SpringBootJacksonConfig.java
+++ open-library-web/src/main/java/org/cqq/oplibrary/web/config/SpringBootJacksonConfig.java
@@ -4,14 +4,17 @@ import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
 import org.cqq.openlibrary.core.jackson.deserializer.LocalDateJacksonDeserializer;
 import org.cqq.openlibrary.core.jackson.deserializer.LocalDateTimeJacksonDeserializer;
 import org.cqq.openlibrary.core.jackson.deserializer.LocalTimeJacksonDeserializer;
+import org.cqq.openlibrary.core.jackson.deserializer.Reserve2BigDecimalsJacksonDeserializer;
 import org.cqq.openlibrary.core.jackson.serializer.ArrayLongSerializer;
 import org.cqq.openlibrary.core.jackson.serializer.ArrayPrimitiveLongSerializer;
 import org.cqq.openlibrary.core.jackson.serializer.LocalDateJacksonSerializer;
 import org.cqq.openlibrary.core.jackson.serializer.LocalDateTimeJacksonSerializer;
 import org.cqq.openlibrary.core.jackson.serializer.LocalTimeJacksonSerializer;
+import org.cqq.openlibrary.core.jackson.serializer.Reserve2BigDecimalsJacksonSerializer;
 import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 
+import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
@@ -25,6 +28,9 @@ public class SpringBootJacksonConfig implements Jackson2ObjectMapperBuilderCusto
 
     @Override
     public void customize(Jackson2ObjectMapperBuilder jacksonObjectMapperBuilder) {
+        // 序列化: 应用到 -> 注入的 ObjectMapper.writeValueAsString & SpringMVC 处理接口出参
+        // 反序列化: 应用到 -> 注入的 ObjectMapper.readValue
+
         // 1. Long 转 String 防止前端接收精度丢失
         jacksonObjectMapperBuilder.serializerByType(Long.TYPE, ToStringSerializer.instance);
         jacksonObjectMapperBuilder.serializerByType(Long.class, ToStringSerializer.instance);
@@ -32,13 +38,16 @@ public class SpringBootJacksonConfig implements Jackson2ObjectMapperBuilderCusto
         jacksonObjectMapperBuilder.serializerByType(long[].class, ArrayPrimitiveLongSerializer.instance);
 
         // 2. 日期序列化
-        // 2.1) 序列化: 被应用到 -> 注入的 ObjectMapper.writeValueAsString & SpringMVC 处理接口出参
         jacksonObjectMapperBuilder.serializerByType(LocalTime.class, LocalTimeJacksonSerializer.instance);
         jacksonObjectMapperBuilder.serializerByType(LocalDate.class, LocalDateJacksonSerializer.instance);
         jacksonObjectMapperBuilder.serializerByType(LocalDateTime.class, LocalDateTimeJacksonSerializer.instance);
-        // 2.2) 反序列化: 被应用到 -> 注入的 ObjectMapper.readValue
+
         jacksonObjectMapperBuilder.deserializerByType(LocalTime.class, LocalTimeJacksonDeserializer.instance);
         jacksonObjectMapperBuilder.deserializerByType(LocalDate.class, LocalDateJacksonDeserializer.instance);
         jacksonObjectMapperBuilder.deserializerByType(LocalDateTime.class, LocalDateTimeJacksonDeserializer.instance);
+
+        // 3. 浮点数
+        jacksonObjectMapperBuilder.serializerByType(BigDecimal.class, Reserve2BigDecimalsJacksonSerializer.instance);
+        jacksonObjectMapperBuilder.deserializerByType(BigDecimal.class, Reserve2BigDecimalsJacksonDeserializer.instance);
     }
 }
