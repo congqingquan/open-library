fce230a20cdd992dd494191f0e602ef30b28942d
congqingquan <17600073620@163.com>
2023-11-30 10:46:28 +0800

    Reserve2BigDecimalsJacksonSerializer / Reserve2BigDecimalsJacksonDeserializer

---
+++ open-library-core/src/main/java/org/cqq/openlibrary/core/jackson/serializer/Reserve2BigDecimalsJacksonSerializer.java
@@ -0,0 +1,34 @@
+package org.cqq.openlibrary.core.jackson.serializer;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.SerializerProvider;
+
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.Objects;
+
+/**
+ * Serializer: BigDecimal(scale: 2, RoundingMode.DOWN).toPlainString() > String
+ *
+ * @author Qingquan.Cong
+ */
+public class Reserve2BigDecimalsJacksonSerializer extends JsonSerializer<BigDecimal> {
+
+    public static final Reserve2BigDecimalsJacksonSerializer instance = new Reserve2BigDecimalsJacksonSerializer();
+
+    @Override
+    public void serialize(BigDecimal value, JsonGenerator generator, SerializerProvider serializers) throws IOException {
+        if (Objects.isNull(value)) {
+            generator.writeNull();
+            return;
+        }
+        generator.writeString(value.setScale(2, RoundingMode.DOWN).toPlainString());
+    }
+
+    @Override
+    public Class<BigDecimal> handledType() {
+        return BigDecimal.class;
+    }
+}
