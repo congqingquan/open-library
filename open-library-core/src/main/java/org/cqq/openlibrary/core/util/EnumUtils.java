573656148291bf9fec780dbef501ae87add06ee2
congqingquan <17600073620@163.com>
2024-01-15 17:57:13 +0800

    utils

--- open-library-core/src/main/java/org/cqq/openlibrary/core/util/EnumUtils.java
+++ open-library-core/src/main/java/org/cqq/openlibrary/core/util/EnumUtils.java
@@ -5,8 +5,6 @@ import java.util.List;
 import java.util.Optional;
 import java.util.function.BiPredicate;
 import java.util.function.Function;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 /**
  * Enum utils
@@ -18,13 +16,6 @@ public class EnumUtils {
     private EnumUtils() {
     }
 
-    /**
-     * 校验枚举是否存在，根据枚举名称
-     * @param enumClass 枚举的类对象
-     * @param name 枚举实例的名称
-     * @param <E> 枚举类型
-     * @return
-     */
     public static <E extends Enum<E>> boolean isValidEnum(Class<E> enumClass, String name) {
         try {
             Enum.valueOf(enumClass, name);
@@ -34,42 +25,29 @@ public class EnumUtils {
         }
     }
 
-    /**
-     * 获取枚举常量列表
-     *
-     * @param enumClass 枚举的类对象
-     * @param <E> 枚举类型
-     * @return
-     */
     public static <E extends Enum<E>> List<E> getEnumConstants(Class<E> enumClass) {
-        return enumClass == null ? new ArrayList<>() : Stream.of(enumClass.getEnumConstants()).collect(Collectors.toList());
+        return enumClass == null ? new ArrayList<>() : CollectionUtils.newArrayList(enumClass.getEnumConstants());
     }
 
     /**
      * 匹配枚举实例
      * @param enumClass 枚举的类对象
+     * @param predicate <Enum, MatchValue>
      * @param matchValue 匹配值
-     * @param predicate 断言式
-     * @return
-     * @param <M> 匹配值类型
-     * @param <E> 枚举类型
      */
-    public static <M, E extends Enum<E>> Optional<E> match(Class<E> enumClass, M matchValue, BiPredicate<M, E> predicate) {
-        return match(enumClass.getEnumConstants(), matchValue, predicate);
+    public static <M, E extends Enum<E>> Optional<E> match(Class<E> enumClass, BiPredicate<E, M> predicate, M matchValue) {
+        return match(enumClass.getEnumConstants(), predicate, matchValue);
     }
 
     /**
      * 匹配枚举实例
      * @param enums 需要匹配的枚举实例
-     * @param matchVal 匹配值
-     * @param predicate 断言式
-     * @return
-     * @param <M> 匹配值类型
-     * @param <E> 枚举类型
+     * @param predicate <Enum, MatchValue>
+     * @param matchValue 匹配值
      */
-    public static <M, E extends Enum<E>> Optional<E> match(E[] enums, M matchVal, BiPredicate<M, E> predicate) {
+    public static <M, E extends Enum<E>> Optional<E> match(E[] enums, BiPredicate<E, M> predicate, M matchValue) {
         for (E enumConstant : enums) {
-            if (predicate.test(matchVal, enumConstant)) {
+            if (predicate.test(enumConstant, matchValue)) {
                 return Optional.of(enumConstant);
             }
         }
@@ -79,28 +57,22 @@ public class EnumUtils {
     /**
      * 匹配枚举实例
      * @param enumClass 枚举的类对象
+     * @param getEnumCompareField 获取 Enum equal 字段
      * @param matchValue 匹配值
-     * @param predicateField Equal断言字段
-     * @return
-     * @param <M> 匹配值类型
-     * @param <E> 枚举类型
      */
-    public static <M, E extends Enum<E>> Optional<E> equalMatch(Class<E> enumClass, M matchValue, Function<E, M> predicateField) {
-        return equalMatch(enumClass.getEnumConstants(), matchValue, predicateField);
+    public static <M, E extends Enum<E>> Optional<E> equalMatch(Class<E> enumClass, Function<E, M> getEnumCompareField, M matchValue) {
+        return equalMatch(enumClass.getEnumConstants(), getEnumCompareField, matchValue);
     }
 
     /**
      * 匹配枚举实例
      * @param enums 需要匹配的枚举实例
-     * @param matchVal 匹配值
-     * @param predicateField Equal断言字段
-     * @return
-     * @param <M> 匹配值类型
-     * @param <E> 枚举类型
+     * @param getEnumCompareField 获取 Enum equal 字段
+     * @param matchValue 匹配值
      */
-    public static <M, E extends Enum<E>> Optional<E> equalMatch(E[] enums, M matchVal, Function<E, M> predicateField) {
+    public static <M, E extends Enum<E>> Optional<E> equalMatch(E[] enums, Function<E, M> getEnumCompareField, M matchValue) {
         for (E enumConstant : enums) {
-            if (predicateField.apply(enumConstant).equals(matchVal)) {
+            if (getEnumCompareField.apply(enumConstant).equals(matchValue)) {
                 return Optional.of(enumConstant);
             }
         }
