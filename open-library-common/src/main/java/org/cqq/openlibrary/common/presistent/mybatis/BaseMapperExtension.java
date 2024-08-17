290165d121befe9b5f496b16a1cd3bb10881f336
qingquan <17600073620@163.com>
2024-08-17 18:15:09 +0800

    1. 调整 pom 2. BaseMapperExtension

---
+++ open-library-common/src/main/java/org/cqq/openlibrary/common/presistent/mybatis/BaseMapperExtension.java
@@ -0,0 +1,14 @@
+package org.cqq.openlibrary.common.presistent.mybatis;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * Mybatis base mapper extension
+ *
+ * @author CongQingquan
+ */
+public interface BaseMapperExtension<T> extends BaseMapper<T> {
+    
+    // TODO
+    
+}
